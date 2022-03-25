ba30e0e00524a539a97b7f16cf5814a3fb5e90e8
Nikola Kelava <nikola.kelava94@gmail.com>
2022-03-25 21:05:27 +0100

    Add elements to note creation area component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import NoteList from './NoteList/NoteList';
+import CreateArea from "./CreateArea/CreateArea";
 import './App.css';
 
 
--- src/components/CreateArea/CreateArea.js
+++ src/components/CreateArea/CreateArea.js
@@ -4,6 +4,11 @@ import React from "react";
 const CreateArea = () => {
     return (
         <div>
+            <form>
+                <input name="title" placeholder="Enter note title here..." />
+                <textarea name="content" placeholder="Take a note..." rows="3" />
+                <button>Add</button>
+            </form>
         </div>
     );
 }
